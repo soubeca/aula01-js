d6d45ff44cd0edc2a05c6c974a6938313616e7d4
Rebeca Souza <rebeca.souza@solutis.com.br>
2024-08-10 19:45:08 -0300

    atividade avaliativa - Aula01

---
+++ script.js
@@ -0,0 +1,16 @@
+let n1 = 7.5 
+let n2 = 8 
+let n3 = 6
+
+const p1 = 5
+const p2 = 3
+const p3 = 2
+
+const somaNota = n1 + n2 + n3 
+const somaPeso = p1 + p2 + p3 
+
+const multiplica = somaNota*somaPeso 
+
+const media = multiplica/somaPeso 
+
+alert(`A sua média é ${media}!`)
